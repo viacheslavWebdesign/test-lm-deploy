2c552a98dc922b727ba9c4a5fb944dda769c80b4
Viacheslav <viacheslav.webdesign@gmail.com>
2024-11-12 13:08:15 +0200

    noindex

--- src/components/headHTML/head-html.jsx
+++ src/components/headHTML/head-html.jsx
@@ -2,13 +2,14 @@ import Head from "next/head";
 
 export const HeadHTML = () => (
   <Head>
-      <title>
-        Розробка сайту у веб-студії Letsmake.site | Професійна розробка сайтів в
-        Україні
-      </title>
-      <meta
-        name="description"
-        content="Розробка сайтів на WordPress, Shopify та Webflow. Дизайн, розробка та підтримка сайтів. Пишіть!"
-      />
-    </Head>
+    <title>
+      Розробка сайту у веб-студії Letsmake.site | Професійна розробка сайтів в
+      Україні
+    </title>
+    <meta
+      name="description"
+      content="Розробка сайтів на WordPress, Shopify та Webflow. Дизайн, розробка та підтримка сайтів. Пишіть!"
+    />
+    <meta name="robots" content="noindex, nofollow" />
+  </Head>
 );
